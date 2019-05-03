664eb7756e3210bd952871cba03dc6b01b488fc4
James Collerton <james.collerton@bbc.co.uk>
2019-05-03 11:02:16 +0100

    Added simple point about hashing with salt

---
+++ salt/index.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto')
+
+// We create a salt and add it to our password. Then when we hash the two we can
+// avoid lookups from dictionary, rainbow and reverse lookup tables.
+crypto.randomBytes(32, function(ex, salt){
+	//log readable string version of the salt
+	console.log('salt: ' + salt.toString('hex'));
+	//proceed to next step: using the salt
+});
