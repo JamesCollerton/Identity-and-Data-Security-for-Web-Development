d6f1b3674e46b6f7ed38e07d63095a95b1c0c6b7
James Collerton <james.collerton@bbc.co.uk>
2019-05-03 17:32:23 +0100

    Need to choose password hashing functions

--- salt/index.js
+++ salt/index.js
@@ -2,6 +2,9 @@ const crypto = require('crypto')
 
 // We create a salt and add it to our password. Then when we hash the two we can
 // avoid lookups from dictionary, rainbow and reverse lookup tables.
+
+// We generate the salt, save the salt and then recombine it with the password when
+// it is submitted.
 crypto.randomBytes(32, function(ex, salt){
 	//log readable string version of the salt
 	console.log('salt: ' + salt.toString('hex'));
