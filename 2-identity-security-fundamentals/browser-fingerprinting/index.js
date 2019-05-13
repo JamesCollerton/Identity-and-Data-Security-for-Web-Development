80ad2dfa69d167a4a96ff45128c78e180b3c8068
James Collerton <james.collerton@bbc.co.uk>
2019-05-13 13:21:02 +0100

    Partway through browser fingerprinting

---
+++ 2-identity-security-fundamentals/browser-fingerprinting/index.js
@@ -0,0 +1,13 @@
+
+
+// The following facets are all considered in browser fingerprinting:
+// 	User Agent
+// 	Plugins
+// 	Fonts
+// 	Videos
+//	Supercookies
+// 	HTTP Accept Header
+// 	Timezone
+// 	Cookies Enabled
+
+
