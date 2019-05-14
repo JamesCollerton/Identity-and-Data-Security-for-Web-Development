745579112e16d9b666069535db04d3e395f78c14
James Collerton <james.collerton@bbc.co.uk>
2019-05-14 11:11:37 +0100

    Got it serving up a static web page

--- 2-identity-security-fundamentals/browser-fingerprinting/server.js
+++ 2-identity-security-fundamentals/browser-fingerprinting/server.js
@@ -4,8 +4,6 @@ const 	querystring = require('querystring'),
 	express = require('express')
 	app = express();
 
-const 	secureConfig = require('./secure-config/secure-config.js')
-
 //support JSON & URL encoded bodies
 app.use(express.static('public'))
 app.use(bodyParser.json());
@@ -16,39 +14,8 @@ app.use(bodyParser.urlencoded({
 // All post requests to this endpoint are handled here.
 app.post('/capture', function (req, res){
 
-	// We know the response contains this, so extract it.
-	var response = req.body['g-recaptcha-response'];
+	console.log("Hello, world")
 
-	console.log(secureConfig.secret_key)
-
-	var verify_data = querystring.stringify({
-		'secret' : secureConfig.secret_key,
-		'response': response
-	});
-
-	//uber access token fetch endpoint
-	var verify_options = {
-		host: 'google.com',
-		path: '/recaptcha/api/siteverify',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-			'Content-Length': verify_data.length
-		}
-	};
-
-	//set up request
-	var post_req = https.request(verify_options, function(result){
-		result.setEncoding('utf8');
-	
-		result.on('data', function (verification){
-			console.log(verification);
-		});
-	});
-
-	//post data
-	post_req.write(verify_data);
-	post_req.end();
 });
 
 app.listen(process.env.PORT || 8000);
