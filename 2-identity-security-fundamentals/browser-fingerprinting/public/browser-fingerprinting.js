46ea6b4aa260e7a1e5f988b4f665948e246282df
James Collerton <james.collerton@bbc.co.uk>
2019-05-14 13:11:10 +0100

    Did some other detection around plugins

--- 2-identity-security-fundamentals/browser-fingerprinting/public/browser-fingerprinting.js
+++ 2-identity-security-fundamentals/browser-fingerprinting/public/browser-fingerprinting.js
@@ -11,3 +11,21 @@
 var agent = navigator.userAgent;
 
 console.log(`User agent ${agent}`)
+
+var offset = new Date().getTimezoneOffset() / 60;
+
+console.log(`Offset ${offset}`)
+
+var width = window.screen.width;
+var height = window.screen.height;
+
+console.log(`Window resolution ${width} x ${height}`)
+
+var plugins = navigator.plugins;
+
+if(plugins.length < 1) {
+	console.log('No plugins')
+}
+for (var i = 0; i < plugins.length; i++){
+	console.log(`Plugin ${plugins[i].name}`);
+}
