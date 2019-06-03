9902311911793b63c293546cc9002bc8fe5ff3d4
James Collerton <james.collerton@bbc.co.uk>
2019-06-03 13:25:31 +0100

    Changed something :

--- 3-oauth2-and-openId-connect/example-express-application/oauth/lib/middleware/authorize.js
+++ 3-oauth2-and-openId-connect/example-express-application/oauth/lib/middleware/authorize.js
@@ -1,35 +1,35 @@
 var Token = require('../models/token');
 
 /*
-    Token middleware. 
-    
+    Token middleware.
+
     The authorize end point is used to give out an authorization token, the token end point
     consumes the authorization token and gives out an access token, this middleware is used to
     make sure that each request has a valid access token associated with it.
 */
 var authorize = function(req, res, next) {
     var accessToken;
-    
+
     // check the authorization header
     if (req.headers.authorization) {
-        
+
         // validate the authorization header
         var parts = req.headers.authorization.split(' ');
-        
+
         if (parts.length < 2) {
             // no access token got provided - cancel
             res.set('WWW-Authenticate', 'Bearer');
             res.sendStatus('401');
             return;
         }
-        
+
         accessToken = parts[1];
-        
+
     } else {
         // access token URI query parameter or entity body
         accessToken = req.query.access_token || req.body.access_token;
     }
-        
+
     if (!accessToken) {
         // no access token got provided - cancel with a 401
     }
@@ -43,11 +43,11 @@ var authorize = function(req, res, next) {
         if (!token) {
             // no token found - cancel
         }
-        
+
         if (token.consumed) {
             // the token got consumed already - cancel
         }
-                
+
         // consume all tokens - including the one used
         Token.update({
             userId: token.userId,
