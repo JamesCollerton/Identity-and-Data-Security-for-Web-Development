0925ef4c26cdf211531de51548886614b9f843b8
James Collerton <jamescollerton@live.co.uk>
2019-06-15 09:44:23 +0100

    All running properly

--- 4-alternate-methods-of-identification/authy-example/app.js
+++ 4-alternate-methods-of-identification/authy-example/app.js
@@ -24,6 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
